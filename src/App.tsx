75847982df1bb3d8f93f171b79ca2554ee7e2924
AnatolyNikol <fanka4ectba@mail.ru>
2024-02-28 13:54:25 +0500

    SuperButton extend button component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div className="App">
             <Box>
                 <StyledBtn>Hello</StyledBtn>
+                <SuperButton>SuperButton</SuperButton>
             </Box>
         </div>
     );
@@ -18,10 +19,15 @@ const StyledBtn = styled.button`
     border: none;
     background-color: brown;
     padding: 10px 20px;
-    border-radius: 5px;
     color: chocolate;
     font-size: 2rem;
     font-weight: bold;
+`;
+
+const SuperButton = styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: darkgoldenrod;
+    color: darkolivegreen;
 `;
 
 const Box = styled.div`
