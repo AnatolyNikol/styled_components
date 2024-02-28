34c01b1c3192478583bc47f2d2e1bd122079433d
AnatolyNikol <fanka4ectba@mail.ru>
2024-02-28 14:32:01 +0500

    use combinators

--- src/App.tsx
+++ src/App.tsx
@@ -76,5 +76,14 @@ const Menu = styled.nav`
     ul {
         list-style: none;
         padding: 0;
+        display: flex;
+
+        li > a {
+            color: green;
+        }
+        
+        li + li {
+            margin-left: 20px;
+        }
     }
 `;
