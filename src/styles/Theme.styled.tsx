a6881f96a81f7bdfe3e991623aaa69ca1bc76137
AnatolyNikol <fanka4ectba@mail.ru>
2024-02-29 18:23:14 +0500

    add them object

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        primary: 'gold',
+        secondary: 'pink',
+        grey: {
+            dark: '#2f2c2c',
+            light: '#d2d2d2'
+        }
+    }
+}
